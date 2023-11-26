1f00a6db2145ee147eb3f8cbcd6c461dc18182c5
i4k1 <99640421+i4k1@users.noreply.github.com>
2023-11-26 16:54:06 +0400

    a script that displays various information in the DWM status line

---
+++ .local/bin/dwmstatus.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# All commands were taken from here: https://dwm.suckless.org/status_monitor/
+
+while true; do
+  xsetroot -name "$(free -h | awk '(NR==2){ print $4 }') $(df -h | awk '{ if ($6 == "/") print $4 }') $(amixer get Master | awk -F'[][]' 'END{ print $2 }') $(date +"%F %u %T")"
+  sleep 2
+done
