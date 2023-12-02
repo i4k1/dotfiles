faeaa6aa8d7c5ff3310f4cdae780fd8811255f34
Ivan <99640421+i4k1@users.noreply.github.com>
2023-12-02 21:54:59 +0400

    aliases fix

--- Profile.ps1
+++ Profile.ps1
@@ -3,8 +3,8 @@
 
 function prompt { return "$([char]27)[91m[$([char]27)[93m$($env:USERNAME)$([char]27)[92m@$([char]27)[94m$($env:COMPUTERNAME) $([char]27)[95m$(get-location)$([char]27)[91m]$([char]27)[0m> " }
 
-function YouTube-DLP { return yt-dlp -Option --add-metadata -i }
-function YouTube-DLP-Audio { return yt -x --audio-format mp3 -f bestaudio/best }
+function YouTube-DLP([string]$YTVIDEOURLS) { return yt-dlp --add-metadata -i "$YTVIDEOURLS" }
+function YouTube-DLP-Audio([string]$YTVIDEOURLS) { return yt-dlp --add-metadata -i -x --audio-format mp3 -f bestaudio/best "$YTVIDEOURLS" }
 
 New-Alias -Name yt -Value YouTube-DLP -Force
 New-Alias -Name yta -Value YouTube-DLP-Audio -Force
