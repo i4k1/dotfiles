4f9505a490fef178da05d6986c2a69c87952f096
Ivan <99640421+i4k1@users.noreply.github.com>
2023-12-04 20:36:59 +0400

    replacing $HOME with ~

--- Profile.ps1
+++ Profile.ps1
@@ -1,10 +1,25 @@
-# 02-12-2023 i4k
+# 04-12-2023 i4k
 # $HOME\Documents\PowerShell\Profile.ps1
 
-function prompt { return "$([char]27)[91m[$([char]27)[93m$($env:USERNAME)$([char]27)[92m@$([char]27)[94m$($env:COMPUTERNAME) $([char]27)[95m$(get-location)$([char]27)[91m]$([char]27)[0m> " }
+function tildehome ([string] $tildelocation) {
+    if ($tildelocation.Contains($HOME)) {
+        return $tilde-location.Replace($HOME, "~")
+    } else {
+        return $tildelocation
+    }
+}
 
-function YouTube-DLP([string]$YTVIDEOURLS) { return yt-dlp --add-metadata -i "$YTVIDEOURLS" }
-function YouTube-DLP-Audio([string]$YTVIDEOURLS) { return yt-dlp --add-metadata -i -x --audio-format mp3 -f bestaudio/best "$YTVIDEOURLS" }
+function prompt {
+    return "$([char]27)[96mPS $([char]27)[92m$($env:USERNAME)@$($env:COMPUTERNAME) $([char]27)[95m$(tildehome($(get-location)))$([char]27)[0m > "
+}
+
+function YouTube-DLP ([string]$YTVIDEOURLS) {
+    return yt-dlp --add-metadata -i "$YTVIDEOURLS"
+}
+
+function YouTube-DLP-Audio ([string]$YTVIDEOURLS) {
+    return yt-dlp --add-metadata -i -x --audio-format mp3 -f bestaudio/best "$YTVIDEOURLS"
+}
 
 New-Alias -Name yt -Value YouTube-DLP -Force
 New-Alias -Name yta -Value YouTube-DLP-Audio -Force
