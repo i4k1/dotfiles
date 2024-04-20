14c8ec0ebf426c24d4361aaa1e4823cb67aa82d1
Ivan <99640421+i4k1@users.noreply.github.com>
2024-04-21 00:13:47 +0400

    Update autorice.sh

--- autorice.sh
+++ autorice.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 # my simple autorice script for debian-based distros
+
+# install packages
+sudo apt update && sudo apt full-upgrade && sudo apt autoremove && sudo apt install build-essential git tmux mc bc vim emacs mg micro ffmpeg dunst
