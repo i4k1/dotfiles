50f0c886d4cd4d759ba3ef89606311248fc9b45b
i4k1 <99640421+i4k1@users.noreply.github.com>
2024-04-29 20:59:10 +0400

    Update autorice.sh

--- autorice.sh
+++ autorice.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# my simple autorice script for debian-based distros
+# automatic script for quick deployment of a working environment
 
 # install packages
 sudo apt update && sudo apt full-upgrade && sudo apt autoremove && sudo apt install build-essential git tmux mc bc vim emacs mg micro ffmpeg dunst
