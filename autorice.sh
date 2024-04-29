dbbf2fd728d4eed978ba820908e7e9ad87da716d
i4k1 <99640421+i4k1@users.noreply.github.com>
2024-04-29 21:28:38 +0400

    add some programs

--- autorice.sh
+++ autorice.sh
@@ -2,4 +2,4 @@
 # automatic script for quick deployment of a working environment
 
 # install packages
-sudo apt update && sudo apt full-upgrade && sudo apt autoremove && sudo apt install build-essential git tmux mc bc vim emacs mg micro ffmpeg dunst
+sudo apt update && sudo apt full-upgrade && sudo apt autoremove && sudo apt install build-essential make gdb nasm yasm fasm git patch man pkg-config tmux mc bc vim emacs nano mg micro ffmpeg dunst libsdl2-dev libx11-dev libxft-dev libxinerama-dev yt-dlp neofetch screenfetch cpufetch dillo lynx htop btop schism curl wget cmatrix bb caca-utils mpv nethack-console crawl ninvaders xorg xterm tint
