936ca0445fea547af6dc557e5e5688419d0c1322
i4k1 <99640421+i4k1@users.noreply.github.com>
2024-05-01 22:52:50 +0400

    Update autodeploy.sh

--- autodeploy.sh
+++ autodeploy.sh
@@ -2,4 +2,4 @@
 # automatic script for quick deployment of a working environment
 
 # install packages
-sudo apt update && sudo apt install build-essential make gdb tcc nasm yasm fasm git patch man pkg-config tmux mc bc vim emacs nano mg micro ffmpeg dunst libsdl2-dev libx11-dev libxft-dev libxinerama-dev yt-dlp neofetch screenfetch cpufetch dillo netsurf lynx htop btop schism curl wget cmatrix bb caca-utils mpv nethack-console crawl ninvaders xorg xterm tint groff mandoc debootstrap vrms gzip bzip2 lzip transmission transmission-cli irssi mutt pass nginx grafx2 busybox
+sudo apt update && sudo apt install build-essential make gdb tcc nasm yasm fasm git patch man pkg-config tmux mc bc vim emacs nano mg micro ffmpeg dunst libsdl2-dev libx11-dev libxft-dev libxinerama-dev yt-dlp neofetch screenfetch cpufetch dillo netsurf-fb netsurf-gtk lynx htop btop schism curl wget cmatrix bb caca-utils mpv nethack-console crawl ninvaders xorg xterm tint groff mandoc debootstrap vrms gzip bzip2 lzip transmission transmission-cli irssi mutt pass nginx grafx2 busybox
