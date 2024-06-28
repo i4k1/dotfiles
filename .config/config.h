85eff0b7ba67a45bc49a3d3ec409e9b8b6045b6b
i4k1 <99640421+i4k1@users.noreply.github.com>
2024-06-28 16:47:05 +0400

    fix dwm config

--- .config/config.h
+++ .config/config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#000000";
@@ -56,8 +56,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "xterm", "-fg", "white", "-bg", "black", "-fa", "\"DejaVu Sans Mono:pixelsize=14:antialias=true:autohint=true\"", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = { "xterm", "-fg", "beige", "-bg", "black", "+sb", "-fa", "\"Liberation Mono:pixelsize=16:antialias=true:autohint=true\"", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
