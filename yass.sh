943ffe0c0977ad457b2b5d1c8983369d812a626c
i4k1 <99640421+i4k1@users.noreply.github.com>
2024-07-02 00:22:40 +0400

    Update yass.sh

--- yass.sh
+++ yass.sh
@@ -52,5 +52,5 @@ rm yass.sh README.md LICENSE
 sudo chmod 777 ~/.local/bin/dwmstatus.sh
 sudo chmod 777 ~/.local/bin/ffwebmenc.sh
 
-echo "if [ -z \"\$DISPLAY\" ] && [ \"\$XDG_VTNR\" = 1 ]; then exec startx; fi" >> ~/.bashrc
+echo "\nif [ -z \"\$DISPLAY\" ] && [ \"\$XDG_VTNR\" = 1 ]; then exec startx; fi\n[ -n \"$XTERM_VERSION\" ] && transset --id \"$WINDOWID\" >/dev/null\n" >> ~/.bashrc
 echo "exec setxkbmap -layout us,ru -option grp:alt_shift_toggle &\nexec ~/.local/bin/dwmstatus.sh &\nexec picom -b -c -f -D 2 &\nexec dunst &\nexec feh --bg-scale --no-fehbg ~/.local/share/bg/waterfalls.jpg &\nexec dwm" >> ~/.xinitrc
