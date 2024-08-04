ddd767f0459cf1b2d4749d1edeea55588b88cd19
i4k1 <99640421+i4k1@users.noreply.github.com>
2024-08-04 15:37:02 +0400

    fix dmenu color

--- .config/config.h
+++ .config/config.h
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-b", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "xterm", "-fg", "beige", "-bg", "black", "+sb", "-tn", "xterm-256color", "-fa", "Liberation Mono:pixelsize=16:antialias=true:autohint=true", NULL };
 
 static const Key keys[] = {
