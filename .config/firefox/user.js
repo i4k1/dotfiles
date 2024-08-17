58f2f8c5b6f1870c3250122325371c4bc0c8f068
i4k1 <99640421+i4k1@users.noreply.github.com>
2024-08-18 00:34:05 +0400

    Update user.js

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -1,3 +1,6 @@
 // soon...
 
 user_pref("browser.compactmode.show", true);
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("extensions.pocket.enabled", false);
